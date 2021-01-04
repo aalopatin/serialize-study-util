3e114e58d874aa12674ac722c799e031ae9462e7
eugenekorolev <100HzJugin@gmail.com>
2021-01-05 00:37:27 +0300

    add for lesson 17

---
+++ src/main/java/homework/v3/HomeWork.java
@@ -0,0 +1,24 @@
+package homework.v3;
+
+
+/**
+ * Задание
+ * 1) Составить файл с JSON-объектом, который "разложен" в пакете homework.v3.classwork.entity.
+ * Определить какой элемент является корневым
+ * Дать имя файлу homework.parameters.json
+ * Сделать файл с минимум пятью элементами
+ * 2) Заполнить его значениями (как пример "parameters.v1.json")
+ * 3) Считать получившийся homework.parameters.json в память
+ * 4) Сериализовать его с помощью встроенного механиза сериализации в файл с именем homework.parameters.ser
+ * 5) Сериализовать его с использованием интерфейса Externalizable в файл с именем homework.parameters.exter
+ * 6) Считать данные из файла homework.parameters.ser в память и сохранить в формате JSON в файл с именем homework.result.ser.parameters.json
+ * 7) Считать данные из файла homework.parameters.exter в память и сохранить в формате JSON в файл с именем homework.result.exter.parameters.json
+ * 8) Убедиться, что файлы homework.result.ser.parameters.json и  homework.result.exter.parameters.json
+ * совпадают с homework.parameters.json
+ * 9) Составленную в п.1 сущность представить в виде xsd-схемы и
+ * выполнить генерацию классов аналогично классам из пакета classwork.entity.jaxb
+ * * можно сделать и с json-схемой, пренципиально механизм не поменяется.
+ * */
+
+public class HomeWork {
+}
